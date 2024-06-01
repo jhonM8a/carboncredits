a00c5ec76ec4eaf7dc4efc15a79b3ca526bcbdee
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-05-31 19:55:26 -0500

    add controller impl

--- companies/src/main/java/com/jochoa/companies/controllers/CompaniesController.java
+++ companies/src/main/java/com/jochoa/companies/controllers/CompaniesController.java
@@ -3,11 +3,16 @@ package com.jochoa.companies.controllers;
 import com.jochoa.companies.dao.CompaniesDAO;
 import com.jochoa.companies.models.Company;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class CompaniesController {
-    CompaniesDAO companies;
+    static CompaniesDAO companies = new CompaniesDAO();
+
+    static {
+        companies.addCompany(new Company(1L,"","",""));
+    }
 
     @GetMapping("/companies")
     public List<Company> getCompanies(){
