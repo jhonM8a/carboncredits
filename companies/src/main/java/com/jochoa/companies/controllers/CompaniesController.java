f524001fc155df0273b7e1f79b635763580294bd
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-06-01 09:00:45 -0500

    todos los methods http de la clase

--- companies/src/main/java/com/jochoa/companies/controllers/CompaniesController.java
+++ companies/src/main/java/com/jochoa/companies/controllers/CompaniesController.java
@@ -2,10 +2,11 @@ package com.jochoa.companies.controllers;
 
 import com.jochoa.companies.dao.CompaniesDAO;
 import com.jochoa.companies.models.Company;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class CompaniesController {
     static CompaniesDAO companies = new CompaniesDAO();
@@ -18,4 +19,25 @@ public class CompaniesController {
     public List<Company> getCompanies(){
         return companies.getCompanies();
     }
+    @PostMapping("/companies")
+    public String addCompany(@RequestBody Company company){
+        companies.addCompany(company);
+        return "ok";
+    }
+    @GetMapping("/companies/{id}")
+    public Optional<Company> getCompany(@PathVariable Long id){
+        return companies.getCompanies().stream().filter(company -> company.getId() == id).findFirst();
+    }
+
+    @PutMapping("/companies/{id}")
+    public String updateCompany(@PathVariable Long id){
+        return "ok";
+    }
+
+    @DeleteMapping
+    public void deleteCompany(@PathVariable Long id){
+
+    }
+
+
 }
