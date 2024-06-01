8346ad45f11eef7c765a184118d42c72aaf2d5c2
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-06-01 12:23:40 -0500

    taller dos

---
+++ tallerdos/src/main/java/com/jochoa/carboncredits/CarboncreditswsApplication.java
@@ -0,0 +1,13 @@
+package com.jochoa.carboncredits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarboncreditswsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarboncreditswsApplication.class, args);
+	}
+
+}
---
+++ tallerdos/src/test/java/com/jochoa/carboncredits/CarboncreditswsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jochoa.carboncredits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarboncreditswsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
