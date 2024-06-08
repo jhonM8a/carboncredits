bd686de3715d8a1982aa258b29e35efae8cf429f
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-06-07 21:20:26 -0500

    resto de codigo

---
+++ tallerdos/src/main/java/com/jochoa/carboncredits/controllers/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.jochoa.carboncredits.controllers;
+import com.jochoa.carboncredits.dtos.LoginResponse;
+import com.jochoa.carboncredits.dtos.LoginUserDto;
+import com.jochoa.carboncredits.dtos.RegisterUserDto;
+import com.jochoa.carboncredits.entities.User;
+import com.jochoa.carboncredits.services.AuthenticationService;
+import com.jochoa.carboncredits.services.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse().setToken(jwtToken).setExpiresIn(jwtService.getExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
+
